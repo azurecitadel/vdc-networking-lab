83551aa25e71f9edb2024c4d1a94b56410259d7f
Richard Cheney <richard.cheney@microsoft.com>
2018-07-23 17:15:39 +0100

    Improved test for at job submission

--- nested/DemoAppConfig.sh
+++ nested/DemoAppConfig.sh
@@ -49,9 +49,13 @@ echo "Updating the labuser .bashrc for coloured prompts"
 [[ ! -f ~labuser/.bashrc.orig ]] && cp -p ~labuser/.bashrc ~labuser/.bashrc.orig
 curl --silent https://raw.githubusercontent.com/azurecitadel/vdc-networking-lab/master/nested/.bashrc > ~labuser/.bashrc
 
-if [[ -f /var/run/reboot-required ]]
+if [ -f /var/run/reboot-required -a "$(whoami)" = "root" ]
 then
     date +"%b %d %H:%M:%S"
-    echo "System restart required"
-    sudo shutdown --no-wall --reboot now
+    echo "System restart required - submitting at job"
+    at now + 1 min <<-EOF
+        shutdown --reboot --no-wall now
+        EOF
 fi
+
+exit 0
