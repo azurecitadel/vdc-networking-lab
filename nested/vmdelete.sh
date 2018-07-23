f0e858f0e65ff576d0a0133b4771054bf9e80606
Richard Cheney <richard.cheney@microsoft.com>
2018-07-23 13:41:05 +0100

    Sort by VM

--- nested/vmdelete.sh
+++ nested/vmdelete.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 az vm delete --yes --resource-group VDC-Spoke1 --name Spoke1-vm1
-az vm delete --yes --resource-group VDC-Spoke1 --name Spoke1-vm2
-az vm delete --yes --resource-group VDC-Spoke2 --name Spoke2-vm1
-az vm delete --yes --resource-group VDC-Spoke2 --name Spoke2-vm2
-
 az disk delete --yes --no-wait --resource-group VDC-Spoke1 --name Spoke1-vm1-os
+
+az vm delete --yes --resource-group VDC-Spoke1 --name Spoke1-vm2
 az disk delete --yes --no-wait --resource-group VDC-Spoke1 --name Spoke1-vm2-os
+
+az vm delete --yes --resource-group VDC-Spoke2 --name Spoke2-vm1
 az disk delete --yes --no-wait --resource-group VDC-Spoke2 --name Spoke2-vm1-os
+
+az vm delete --yes --resource-group VDC-Spoke2 --name Spoke2-vm2
 az disk delete --yes --no-wait --resource-group VDC-Spoke2 --name Spoke2-vm2-os
