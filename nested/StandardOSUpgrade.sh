f2c5f6326576c1b9e2d460484e9750163045ba81
Richard Cheney <richard.cheney@microsoft.com>
2018-07-27 17:56:57 +0100

    Added reboot

--- nested/StandardOSUpgrade.sh
+++ nested/StandardOSUpgrade.sh
@@ -14,3 +14,14 @@ date +"%b %d %H:%M:%S"
 echo "Updating the labuser .bashrc for coloured prompts"
 [[ ! -f ~labuser/.bashrc.orig ]] && cp -p ~labuser/.bashrc ~labuser/.bashrc.orig
 curl --silent https://raw.githubusercontent.com/azurecitadel/vdc-networking-lab/master/nested/.bashrc > ~labuser/.bashrc
+
+if [ -f /var/run/reboot-required -a "$(whoami)" = "root" ]
+then
+    date +"%b %d %H:%M:%S"
+    echo "System restart required - submitting at job"
+    at now + 1 min <<-EOF
+	shutdown --reboot --no-wall now
+	EOF
+fi
+
+exit 0
